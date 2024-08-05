fbf8a1d19a1c79abe93460477a643bd19306c1a1
Lucas <lucasgusso28@gmail.com>
2024-08-05 17:44:05 -0300

    teste

---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,28 @@
+package br.com.alura.screenmatch.modelos;
+
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+public class Filme extends Titulo implements Classificavel {
+    private String diretor;
+    public Filme(String nome, int anoDeLancamento) {
+        super(nome, anoDeLancamento);
+    }
+
+    @Override
+    public int getClassificacao() {
+        return (int) pegaMedia()/2;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme: " + this.getNome() + "(" + this.getAnoDeLancamento() + ")";
+    }
+}
---
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -0,0 +1,71 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Titulo implements Comparable<Titulo>{
+
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public Titulo(String nome, int anoDeLancamento) {
+        this.nome = nome;
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + this.nome);
+        System.out.println("Ano de lançamento: " + this.anoDeLancamento);
+    }
+
+    public void avalia(double nota) {
+        this.somaDasAvaliacoes += nota;
+        totalDeAvaliacoes ++;
+    }
+
+    public double pegaMedia(){
+        return this.somaDasAvaliacoes / this.totalDeAvaliacoes;
+    }
+
+    @Override
+    public int compareTo(Titulo outro) {
+        return this.getNome().compareTo(outro.getNome());
+    }
+}
